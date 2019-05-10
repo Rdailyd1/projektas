704e8c7358fba1abdfb09d10c534f92f56855f4f
Atgaras <goferis.e@gmail.com>
2019-05-10 18:46:22 +0300

    Užpildytos formulės

--- Projektas/Program.cs
+++ Projektas/Program.cs
@@ -10,9 +10,11 @@ namespace Projektas
     {
         static void Main(string[] args)
         {
-            double rutulio_plotas = 0;
-            double rutulio_turis = 0;
-            double izamb_ilgis = 0;
+            double rutulio_plotas = 4 * Math.PI * 6371*6371);
+            double rutulio_turis = 4/3 * Math.PI* 6371*6371*6371;
+            double izamb_ilgis = 10 * 10 * 15 * 15;
+
+            
         }
     }
 }
